ed4dbd1ea21dfd63a591b62f88eb6b24fa1cdbf7
sultanmehmood84 <ghdg786786>
2016-09-30 10:40:49 +0500

    Changes

--- src/android/PhoneDialer.java
+++ src/android/PhoneDialer.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package com.teamnemitoff.phonedialer;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.json.JSONException;
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-
-public class PhoneDialer extends CordovaPlugin {
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-	    try {
-	    	String phoneNumber = args.getString(0);
-		String encodedPhonenumber = "+19197257941,43188,#,29493,#";
-	    	Uri uri = new Uri.Builder().scheme("tel").encodedPath(phonenumber).build();
-            Intent callIntent = new Intent(Intent.ACTION_CALL);
-            callIntent.setData(uri);
-            this.cordova.getActivity().startActivity(callIntent);
-            callbackContext.success();
-            return true;
-        } catch (Exception e) {
-        	String msg = "Exception Dialing Phone Number: " + e.getMessage();
-        	System.err.println(msg);
-	        callbackContext.error(msg);
-	        return false;
-        }
-	}
-=======
 package com.teamnemitoff.phonedialer;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -58,5 +27,4 @@ public class PhoneDialer extends CordovaPlugin {
 	        return false;
         }
 	}
->>>>>>> debbd7ce4671a18362d435ad06426bef7d099b1c
 }
